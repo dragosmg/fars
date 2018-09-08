00a4ff374119fefe97208f3137d43c15104fdfb8
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2018-09-08 21:25:42 +0100

    added one test for fars_read

--- tests/testthat/test-fars_functions.R
+++ tests/testthat/test-fars_functions.R
@@ -4,3 +4,12 @@ test_that("make_filename works", {
   expect_equal(make_filename(2014), "accident_2014.csv.bz2")
   expect_equal(make_filename(2013), "accident_2013.csv.bz2")
 })
+
+test_that("fars_read works as expected", {
+  path <- paste0(system.file(package = "fars"), "/extdata")
+  good_file <- paste0(path, "/accident_2014.csv.bz2")
+  bad_file <- paste0(path, "/accident_2012.csv.bz2")
+  expect_error(fars_read(bad_file))
+  expect_success(fars_read(good_file))
+})
+
